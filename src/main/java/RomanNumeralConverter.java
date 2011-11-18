2b88220c874d6930d93a63c4ff5d6e70a6049d4c
Kai Hoelscher <Kai.Hoelscher@coremedia.com>
2011-11-18 10:57:57 +0100

    Initial import.

---
+++ src/main/java/RomanNumeralConverter.java
@@ -0,0 +1,7 @@
+/**
+ * Converts integers into Roman numerals (e.g. 7 -> "VII" ).
+ *
+ * You might want to have a look at the <a href="http://codingdojo.org/cgi-bin/wiki.pl?KataRomanNumerals">description of this CodeKata</a> at codingdojo.org.
+ */
+public class RomanNumeralConverter {
+}

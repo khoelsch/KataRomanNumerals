8a685416cac5fac193f7cacedb433eef018a703b
Kai Hoelscher <Kai.Hoelscher@coremedia.com>
2011-11-24 16:46:49 +0100

    fix SHIFT T

--- src/main/java/com/coremedia/codekata/RomanNumeralConverter.java
+++ src/main/java/com/coremedia/codekata/RomanNumeralConverter.java
@@ -1,7 +1,10 @@
+package com.coremedia.codekata;
+
 /**
  * Converts integers into Roman numerals (e.g. 7 -> "VII" ).
  *
  * You might want to have a look at the <a href="http://codingdojo.org/cgi-bin/wiki.pl?KataRomanNumerals">description of this CodeKata</a> at codingdojo.org.
  */
 public class RomanNumeralConverter {
+
 }
---
+++ src/test/java/com/coremedia/codekata/RomanNumeralConverterTest.java
@@ -0,0 +1,10 @@
+package com.coremedia.codekata;
+
+import org.junit.Test;
+
+public class RomanNumeralConverterTest {
+  @Test
+  public void foo() {
+
+  }
+}
